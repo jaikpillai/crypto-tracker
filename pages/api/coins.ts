7d1b2104bea928aa726fa153204df09476f0052e
Jai <jaipilllai@gmail.com>
2022-08-14 14:50:28 +0530

    cors testing

--- pages/api/coins.ts
+++ pages/api/coins.ts
@@ -4,7 +4,7 @@ import Cors from "cors";
 // Initializing the cors middleware
 // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
 const cors = Cors({
-  origin: ["https://www.google.com", "http://localhost:3000/"],
+  origin: ["https://www.google.com", "http://localhost:3000"],
   methods: ["POST", "GET", "HEAD"],
 });
 
